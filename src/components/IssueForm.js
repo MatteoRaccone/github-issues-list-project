0981d4186a0ed1ae8f5b3ced96fd41f49dfea77c
matteoraccone <matteo.raccone@gmail.com>
2021-01-15 13:01:36 +0100

    converted issue form with hooks v2

--- src/components/IssueForm.js
+++ src/components/IssueForm.js
@@ -5,14 +5,14 @@ import { Link } from "react-router-dom";
 
 
 export function IssueForm(props) {
-    const { value:Author, bind:bindAuthor, reset:resetFirstName } = useInput('');
-    const { value:Body, bind:bindBody, reset:resetLastName } = useInput('');
+    const { value:Author, bind:bindAuthor, reset:resetAuthor } = useInput('');
+    const { value:Body, bind:bindBody, reset:resetBody } = useInput('');
     
-    const handleSubmit = (evt) => {
-        evt.preventDefault();
+    const handleSubmit = (event) => {
+        event.preventDefault();
         alert(`Submitting Name ${Author} ${Body}`);
-        resetFirstName();
-        resetLastName();
+        resetAuthor();
+        resetBody();
     }
     return (
       <div className ="container mt-5 mb-5 newissue-container p-3">
